b3bac522c30bcf1522de06a8e76cd6bca5b56e27
himiko <antonin2809@gmail.com>
2021-04-07 16:22:09 +0200

    Initial commit

---
+++ src/test/java/fr/unilim/iut/spaceinvaders/AppTest.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.spaceinvaders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
