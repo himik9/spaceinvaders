93c0b3dc6529a5411810e9f160a3e9c80e54d476
himiko <antonin2809@gmail.com>
2021-04-07 17:34:16 +0200

    Commit fin de TP

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.spaceinvaders;
+
+public class SpaceInvaders {
+
+  int longueur;
+  int hauteur;
+
+  public SpaceInvaders(int longueur, int hauteur) {
+    this.longueur = longueur;
+    this.hauteur = hauteur;
+  }
+
+}
